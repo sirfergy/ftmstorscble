390b8af22d154748dd5ad0d6e2864fe12d663cfe
Sean Ferguson <sean@thefergies.net>
2025-02-05 20:56:55 -0800

    Refactor into methods and add a 5 minute disconnect

--- src/services/ftms.ts
+++ src/services/ftms.ts
@@ -1,4 +1,4 @@
-import noble from "@abandonware/noble";
+import noble, { Peripheral } from "@abandonware/noble";
 import Debug from "debug";
 const debug = Debug("ftms");
 
@@ -21,84 +21,102 @@ const ftmsFlags = {
 export class FtmsService {
     private discovered = false;
     private callback?: (speedMetersPerSecond: number) => void;
+    private lastDataReceived = Date.now();
+    private peripheral: Peripheral;
 
     constructor() {
-        noble.on('stateChange', (state) => {
-            debug(`State change: ${state}`);
+        noble.on('stateChange', this.onStateChange);
+        noble.on('discover', this.onDiscover);
 
-            if (state == "poweredOn") {
-                noble.startScanning(["1826"], true, (error) => {
-                    if (error) {
-                        debug(error);
-                    }
-                });
-            }
-            else if (state == "poweredOff") {
+        setInterval(() => {
+            if (Date.now() - this.lastDataReceived > 5 * 60 * 1000) {
+                debug("Disconnecting due to inactivity");
                 noble.stopScanning();
+                noble.removeAllListeners();
+                noble.resetAsync();
             }
-        });
-
-        noble.on('discover', async (peripheral) => {
-            debug(`Discovered: ${peripheral.advertisement.localName}`);
-
-            if (!this.discovered && peripheral.advertisement.localName && peripheral.advertisement.localName.includes("HORIZON")) {
-                this.discovered = true;
-
-                await noble.stopScanningAsync();
-                await peripheral.connectAsync();
-
-                const { services } = await peripheral.discoverSomeServicesAndCharacteristicsAsync(["1826"], ["2acd"]);
-
-                const ftms = services.find(s => s.uuid == "1826")!;
-                const treadmill = ftms.characteristics.find(c => c.uuid.toLowerCase() == "2acd")!;
-
-                await treadmill.subscribeAsync();
-
-                peripheral.on('disconnect', () => {
-                    debug("Disconnected");
-                    this.discovered = false;
-                });
-
-                treadmill.on('data', (data, isNotification) => {
-                    const flags = data.readUInt16LE();
-
-                    if ((flags & ftmsFlags.moreData) !== ftmsFlags.moreData) {
-                        const speedInDekametersPerHour = data.readUInt16LE(2);
-                        debug(`Instantaneous speed: ${speedInDekametersPerHour}`);
-
-                        const speedMetersPerSecond = speedInDekametersPerHour * 10 / 3600;
-
-                        this.callback && this.callback(speedMetersPerSecond);
-
-                        return;
-                    }
-
-                    const averageSpeed = data.readUInt16LE(2);
-                    debug(`Average speed: ${averageSpeed}`);
-
-                    const totalDistance = data.readUIntLE(4, 3);
-                    debug(`Total distance: ${totalDistance}`);
-
-                    const inclination = data.readInt16LE(7);
-                    const rampAngleSetting = data.readInt16LE(9);
-                    debug(`Inclination: ${inclination} - ${rampAngleSetting}`);
-
-                    const totalEnergy = data.readUInt16LE(11);
-                    const energyPerHour = data.readUInt16LE(13); // actually total energy
-                    const energyPerMinute = data.readUIntLE(15, 1);
-                    debug(`Expended energy: ${totalEnergy} - ${energyPerHour} - ${energyPerMinute}`);
-
-                    const heartRate = data.readUIntLE(16, 1);
-                    debug(`Heart rate: ${heartRate}`);
-
-                    const elapsedTime = data.readUInt16LE(17);
-                    debug(`Elapsed time: ${elapsedTime}`);
-                });
-            }
-        });
+        }, 1000);
     }
 
-    public subscribeToFtmsMessages(callback: (speedMetersPerSecond: number) => void) {
+    public subscribeToFtmsMessages(callback: (speedMetersPerSecond: number) => void): void {
         this.callback = callback;
+    }
+
+    private onStateChange(state: string): void {
+        debug(`State change: ${state}`);
+
+        if (state == "poweredOn") {
+            noble.startScanning(["1826"], true, (error) => {
+                if (error) {
+                    debug(error);
+                }
+            });
+        }
+        else if (state == "poweredOff") {
+            noble.stopScanning();
+        }
+    }
+
+    private async onDiscover(peripheral: Peripheral): Promise<void> {
+        debug(`Discovered: ${peripheral.advertisement.localName}`);
+
+        if (!this.discovered && peripheral.advertisement.localName && peripheral.advertisement.localName.includes("HORIZON")) {
+            this.discovered = true;
+            this.peripheral = peripheral;
+
+            await noble.stopScanningAsync();
+            await peripheral.connectAsync();
+
+            const { services } = await peripheral.discoverSomeServicesAndCharacteristicsAsync(["1826"], ["2acd"]);
+
+            const ftms = services.find(s => s.uuid == "1826")!;
+            const treadmill = ftms.characteristics.find(c => c.uuid.toLowerCase() == "2acd")!;
+
+            await treadmill.subscribeAsync();
+
+            peripheral.on('disconnect', () => {
+                debug("Disconnected");
+                this.discovered = false;
+            });
+
+            treadmill.on('data', this.onTreadmillData);
+        }
+    }
+
+    private onTreadmillData(data: Buffer, isNotification: boolean): void {
+        this.lastDataReceived = Date.now();
+        const flags = data.readUInt16LE();
+
+        if ((flags & ftmsFlags.moreData) !== ftmsFlags.moreData) {
+            const speedInDekametersPerHour = data.readUInt16LE(2);
+            debug(`Instantaneous speed: ${speedInDekametersPerHour}`);
+
+            const speedMetersPerSecond = speedInDekametersPerHour * 10 / 3600;
+
+            this.callback && this.callback(speedMetersPerSecond);
+
+            return;
+        }
+
+        const averageSpeed = data.readUInt16LE(2);
+        debug(`Average speed: ${averageSpeed}`);
+
+        const totalDistance = data.readUIntLE(4, 3);
+        debug(`Total distance: ${totalDistance}`);
+
+        const inclination = data.readInt16LE(7);
+        const rampAngleSetting = data.readInt16LE(9);
+        debug(`Inclination: ${inclination} - ${rampAngleSetting}`);
+
+        const totalEnergy = data.readUInt16LE(11);
+        const energyPerHour = data.readUInt16LE(13); // actually total energy
+        const energyPerMinute = data.readUIntLE(15, 1);
+        debug(`Expended energy: ${totalEnergy} - ${energyPerHour} - ${energyPerMinute}`);
+
+        const heartRate = data.readUIntLE(16, 1);
+        debug(`Heart rate: ${heartRate}`);
+
+        const elapsedTime = data.readUInt16LE(17);
+        debug(`Elapsed time: ${elapsedTime}`);
     }
 }
